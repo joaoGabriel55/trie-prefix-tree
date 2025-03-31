1a65ff8d99900316df3e2253737b9b4060f3bb64
João Gabriel Quaresma de Almeida <joaoGabriel55>
2025-03-31 08:49:07 -0300

    feat: implement FlagWordsController

--- app/controllers/flag_words_controller.rb
+++ app/controllers/flag_words_controller.rb
@@ -1,7 +1,13 @@
 class FlagWordsController < ApplicationController
     def flag
-        service = FlagWordsService.new(text: params[:text])
+        service = FlagWordsService.new(text: params[:text], forbidden_words:)
 
         render json: { flag_words: service.flag_words }, status: :ok
+    end
+
+    private
+
+    def forbidden_words
+        %w[foo bar baz]
     end
 end
